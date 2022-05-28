37095f99916214c27af1ba89ba2359876704233a
BAmado753 <65674008+BAmado753@users.noreply.github.com>
2022-05-28 18:06:32 +0200

    Actualización vida

---
+++ PracticaEM21-22 v1.1/Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,51 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+public class Bullet : NetworkBehaviour {
+
+    #region Variables
+
+
+    public ulong owner;
+
+    #endregion
+
+    #region Métodos
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        //Si es el servidor, este comprueba si la bala ha colisionado con otro jugador o con plataformas
+
+        if (IsServer) {
+
+            if (collision.gameObject.tag == "Platform") {
+                DestruirBalas();
+            }
+            else if (collision.gameObject.tag == "Player")
+            {
+                //Si la bala contacta con otro pj que no sea el propietario desaparece la bala y hace daño
+                if (owner != collision.GetComponent<Player>().OwnerClientId)
+                {
+                    DestruirBalas();
+                    collision.GetComponent<Player>().DamageDisparo();
+                }
+
+            }
+
+
+
+
+        }
+
+    }
+
+
+    public void DestruirBalas() {
+
+        //Destroy(gameObject);--> en local
+        GetComponent<NetworkObject>().Despawn();
+
+    }
+
+    #endregion
+}
